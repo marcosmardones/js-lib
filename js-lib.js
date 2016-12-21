6c7bbe243f47c3687461dedfa9821437b3ff9e42
marcosmardones <marcos.mardones@gmail.com>
2016-12-21 15:08:00 -0300

    Add files via upload

--- js-lib.js
+++ js-lib.js
@@ -71,3 +71,17 @@ function ajax(url,parametros,callback_done,callback_fail,callback_always){
 		callback_always(data)
 	});
 }
+
+function formatearNumeroConDecimales(num) {
+	separador = "."; // separador para los miles
+	sepDecimal = ','; // separador para los decimales
+	num += '';
+	var splitStr = num.split('.');
+	var splitLeft = splitStr[0];
+	var splitRight = splitStr.length > 1 ? this.sepDecimal + splitStr[1] : '';
+	var regx = /(\d+)(\d{3})/;
+	while (regx.test(splitLeft)) {
+		splitLeft = splitLeft.replace(regx, '$1' + this.separador + '$2');
+	}
+	return splitLeft + splitRight;
+}
